31613ecb509ad8581602faf41352893bf1e06741
MatdMhtd <amintaghavi96@gmail.com>
2023-10-12 18:26:10 +0330

    added my_string class

---
+++ lib/my_strings.dart
@@ -0,0 +1,7 @@
+class MyStrings {
+  static const String viewHotestBlog = "مشاهده داغ ترین نوشته‌ها";
+  static const String viewHotestPodcast = "مشاهده داغ ترین پادکست‌ها";
+  static const String relatedArticale = "نوشته‌های مرتبط";
+  static const String submitArticale = "ارسال مقاله";
+  static const String submitPodcast = "ارسال پادکست";
+}
